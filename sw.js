ad5cfce9640932e5de4727ae2ced2a1a3954a759
Didi-Bz <54994948+Didi-Bz@users.noreply.github.com>
2026-06-05 20:49:03 +0200

    maj pwa

--- sw.js
+++ sw.js
@@ -1,27 +1,21 @@
-const CACHE = 'vitro-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  'https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Mono:wght@400;500&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
-];
+const CACHE = 'vitro-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(['./'])).catch(() => {})
-  );
-  self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(['/Vitro/', '/Vitro/index.html'])).catch(() => {})
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+    e.waitUntil(caches.keys().then(keys =>
+        Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ));
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
 });
